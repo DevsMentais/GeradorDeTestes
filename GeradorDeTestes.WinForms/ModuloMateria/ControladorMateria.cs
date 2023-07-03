f8ff6431445045322c09830b43f7034b46e2b6c6
Bruno <Cliente@DESKTOP-2TBK80U>
2023-07-03 18:06:12 -0300

    Implementado o módulo matéria

--- GeradorDeTestes.WinForms/ModuloMateria/ControladorMateria.cs
+++ GeradorDeTestes.WinForms/ModuloMateria/ControladorMateria.cs
@@ -1,43 +1,111 @@
-﻿using GeradorDeTestes.WinForms.Compartilhado;
+﻿using GeradorDeTestes.Dominio.ModuloDisciplina;
+using GeradorDeTestes.Dominio.ModuloMateria;
+using GeradorDeTestes.WinForms.Compartilhado;
 
 namespace GeradorDeTestes.WinForms.ModuloMateria
 {
     public class ControladorMateria : ControladorBase
     {
-        public override string ToolTipInserir => throw new NotImplementedException();
+        private IRepositorioDisciplina repositorioDisciplina;
+        private IRepositorioMateria repositorioMateria;
+        private TabelaMateriaControl tabelaMateria;
 
-        public override string ToolTipEditar => throw new NotImplementedException();
-
-        public override string ToolTipExcluir => throw new NotImplementedException();
-
-        public override void ApresentarMensagem(string mensagem, string titulo)
+        public ControladorMateria(IRepositorioDisciplina repositorioDisciplina, IRepositorioMateria repositorioMateria)
         {
-            throw new NotImplementedException();
+            this.repositorioDisciplina = repositorioDisciplina;
+            this.repositorioMateria = repositorioMateria;
+        }
+
+        public override string ToolTipInserir => "Inserir Matéria";
+
+        public override string ToolTipEditar => "Editar Matéria";
+
+        public override string ToolTipExcluir => "Excluir Matéria";
+
+        public override void Inserir()
+        {
+            TelaMateriaForm telaMateriaForm = new TelaMateriaForm(repositorioDisciplina.SelecionarTodos());
+
+            DialogResult opcaoEscolhida = telaMateriaForm.ShowDialog();
+
+            if(opcaoEscolhida == DialogResult.OK)
+            {
+                Materia materia = telaMateriaForm.ObterMateria();
+
+                repositorioMateria.Inserir(materia);
+            }
+
+            CarregarMaterias();
         }
 
         public override void Editar()
         {
-            throw new NotImplementedException();
+            TelaMateriaForm telaMateriaForm = new TelaMateriaForm(repositorioDisciplina.SelecionarTodos());
+
+            Materia materiaSelecionada = ObterMateriaSelecionada();
+
+            telaMateriaForm.ConfigurarTela(materiaSelecionada);
+
+            DialogResult opcaoEscolhida = telaMateriaForm.ShowDialog();
+
+            if(opcaoEscolhida == DialogResult.OK)
+            {
+                Materia materia = telaMateriaForm.ObterMateria();
+
+                repositorioMateria.Editar(materia.id, materia);
+            }
+
+            CarregarMaterias();
         }
 
+        
         public override void Excluir()
         {
-            throw new NotImplementedException();
+            Materia materiaSelecionada = ObterMateriaSelecionada();
+
+            DialogResult opcaoEscolhida = MessageBox.Show($"Deseja excluir a matéria {materiaSelecionada.Nome}?", "Exclusão de Matérias",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if(opcaoEscolhida == DialogResult.OK )
+            {
+                repositorioMateria.Excluir(materiaSelecionada);
+            }
+
+            CarregarMaterias();
         }
 
-        public override void Inserir()
+        private void CarregarMaterias()
         {
-            throw new NotImplementedException();
+            List<Materia> materias = repositorioMateria.SelecionarTodos();
+
+            tabelaMateria.AtualizarRegistros(materias);
+        }
+
+        public override void ApresentarMensagem(string mensagem, string titulo)
+        {
+            MessageBox.Show(mensagem, titulo, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
         }
 
         public override UserControl ObterListagem()
         {
-            throw new NotImplementedException();
+            if(tabelaMateria == null)
+                tabelaMateria = new TabelaMateriaControl();
+
+            CarregarMaterias();
+
+            return tabelaMateria;
+        }
+
+        private Materia ObterMateriaSelecionada()
+        {
+            int id = tabelaMateria.ObterIdSelecionado();
+
+            return repositorioMateria.SelecionarPorId(id); 
         }
 
         public override string ObterTipoCadastro()
         {
-            throw new NotImplementedException();
+            return "Cadastro de Matérias";
         }
     }
 }
